acff84ae5a93f3f926bf47b7eb295e792a2a2784
ThanhPhongsof <ttphongletter@gmail.com>
2022-05-06 11:45:19 +0700

    Styling and routing for Dashboard page

---
+++ react-monkey-bloging/.history/src/module/dashboard/DashboardHeader_20220506112644.js
@@ -0,0 +1,23 @@
+import { Button } from "components/button";
+import React from "react";
+import styled from "styled-components";
+
+const DashboardHeaderStyles = styled.div``;
+
+const DashboardHeader = () => {
+  return (
+    <DashboardHeaderStyles>
+      <Button to="/dashboard" className="header-button">
+        Write new post
+      </Button>
+      <div className="header-avatar">
+        <img
+          src="https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80"
+          alt="avatar"
+        />
+      </div>
+    </DashboardHeaderStyles>
+  );
+};
+
+export default DashboardHeader;
---
+++ react-monkey-bloging/.history/src/module/dashboard/DashboardLayout_20220506111600.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+import DashboardHeader from "./DashboardHeader";
+
+const DashboardLayoutStyles = styled.div``;
+
+const DashboardLayout = () => {
+  return (
+    <DashboardLayoutStyles>
+      <DashboardHeader></DashboardHeader>
+    </DashboardLayoutStyles>
+  );
+};
+
+export default DashboardLayout;
